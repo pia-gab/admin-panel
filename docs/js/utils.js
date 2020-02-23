59d869d73a92a6f2ffd8eb156a562347115c2556
gg <piasek.gaba@gmail.com>
2020-02-23 19:44:59 +0100

    add docs folder

---
+++ docs/js/utils.js
@@ -0,0 +1,7 @@
+export const utils = {}; // eslint-disable-line no-unused-vars
+
+utils.minusDays = function(dateStr, days){
+  const dateObj = new Date(dateStr);
+  dateObj.setDate(dateObj.getDate() - days);
+  return dateObj;
+};
